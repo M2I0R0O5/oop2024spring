938276b6e3242d4c96d3ec8a2d89954f3b93c2fd
M2I0R0O5 <149790660+M2I0R0O5@users.noreply.github.com>
2024-03-25 11:51:32 +0400

    Update CoffeeShop.java

--- quizes/q2/mirian_charaia_1/computing/CoffeeShop.java
+++ quizes/q2/mirian_charaia_1/computing/CoffeeShop.java
@@ -6,19 +6,23 @@ public class CoffeeShop {
         int pricePerCoffee = 250; // tetri
         int numCoffeeSold = 100;
         int totalCostOfBeans = 15000; // tetri
-        int totalExpenses;
+        int tax = 34; // lari
+        int water = 31; // tetri
+        int gas = 288; // tetri
+        int electricity = 79; // lari
+        int parking = 26; // lari
 
-        // TODO 1
-        // include all the expences that are provided in the expences.txt file
+        int otherExpenses = tax*100 + water + gas + electricity*100 + parking*100;
 
-        double profit = calculateProfit(pricePerCoffee, numCoffeeSold, totalCostOfBeans, otherExpenses);
+        double profitInLaris = calculateProfit(pricePerCoffee, numCoffeeSold, totalCostOfBeans, otherExpenses);
+        System.out.println("Profit in lari: " + profitInLaris/100);
     }
 
     public static double calculateProfit(int pricePerCoffee, int numCoffeeSold, int totalCostOfBeans, int otherExpenses) {
+        double totalIncome = pricePerCoffee * numCoffeeSold ; // Convert price to lari
+        double totalCost = (totalCostOfBeans + otherExpenses); // Convert cost of beans to lari
 
-        // TODO 2
-        // Finish this function
-
+        double profitInLaris = totalIncome - totalCost;
         return profitInLaris;
     }
 }
